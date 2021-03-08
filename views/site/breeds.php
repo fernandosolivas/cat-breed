5c4e00e7cf7a6f406833ef49b0001f8a01149662
Fernando Soliva <fernando.soliva@loft.com.br>
2021-03-08 05:03:44 -0300

    feat: add image placeholder when there is no image

--- views/site/breeds.php
+++ views/site/breeds.php
@@ -6,6 +6,8 @@
         <div class="col-sm-12  col-md-5 col-lg-5 card">
             <?php if (property_exists($breed, 'image') && property_exists($breed->image, 'url')) { ?>
                 <img class="img-fluid card-img-top" src="<?php echo $breed->image->url ?>" alt="Card image cap">
+            <?php } else { ?>
+                <img alt="cat image" class="img-fluid card-img=top" src="https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png" />
             <?php } ?>
             <div class="card-body card-content">
                 <h5 class="card-title"><?= \yii\helpers\Html::encode("{$breed->name}") ?></h5>
